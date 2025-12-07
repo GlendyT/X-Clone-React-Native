0ecf7c2aab099a77e12840148e17f0c6b98d2c07
GlendyT <glendytuyuc@gmail.com>
2025-12-06 18:26:52 -0600

    adding the backend notification routes and rate limiting

---
+++ backend/src/config/arcjet.js
@@ -0,0 +1,30 @@
+import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
+import { ENV } from "./env.js";
+
+// initialize Arcjet with security rules
+export const aj = arcjet({
+  key: ENV.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    // shield protects your app from common attacks e.g SQL injection, XXS, CSRF attacks
+    shield({ model: "LIVE" }),
+
+    // bot detection - block all bots except search engines
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY: SEARCH_ENGINE",
+        // ALLOW LEGITIMATE SEARCH ENGINE BOTS
+        // see full list at https://arcjet.com/bot-list
+      ],
+    }),
+
+    //rate limiting with token bucket algorithm
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 10, // tokens added per interval
+      interval: 10, // interval in seconds (10 seconds)
+      capacity: 15, // maximum tokens in bucked
+    }),
+  ],
+});
