dec53c5a555362dd0117498bd51ec14b8a30c1b7
GlendyT <glendytuyuc@gmail.com>
2025-12-08 17:39:26 -0600

    adding rectnativewind and the first pages of the app

---
+++ mobile/app/index.tsx
@@ -0,0 +1,10 @@
+import {  Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Edit / index.tsx to edit this creeen</Text>
+    </View>
+  )
+}
+
