300de03f92fa742d056ffdd9b8d5f70d026dbdfa
GlendyT <glendytuyuc@gmail.com>
2025-12-09 11:59:14 -0600

    adding the tab navigator setup

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -6,11 +6,8 @@ import "../global.css";
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Stack>
-        <Stack.Screen
-          name="(auth)"
-          options={{ headerShown: false }}
-        />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       </Stack>
     </ClerkProvider>
   );
